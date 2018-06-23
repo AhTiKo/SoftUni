979c3b85a2d507a5f15ab241e2fa5e354d65ba97
AhTiKo <37878642+AhTiKo@users.noreply.github.com>
2018-06-23 22:54:05 +0300

    Add files via upload

---
+++ QA/EmagTest.cs
@@ -0,0 +1,40 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmagTest
+{
+    public class Test
+    {
+        
+        IWebDriver driver = new ChromeDriver();
+        
+        [Test]
+        public void test()
+        {
+            driver.Url = "https://www.emag.bg/homepage";
+            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(60));
+            IWebElement searchBox = wait.Until<IWebElement>((w) => { return w.FindElement(By.Id("searchboxTrigger")); });
+            searchBox.Clear();
+            searchBox.SendKeys("Тостер Star-Light TS-800");
+            searchBox.Submit();
+
+            IWebElement addProduct = driver.FindElement(By.XPath("//*[@id=\"card_grid\"]/div[1]/div[2]/div/div[3]/div[3]/form/button"));
+            addProduct.Click();
+            addProduct.Submit();
+
+            IWebElement basketElement = driver.FindElement(By.XPath("//*[@id=\"vendorsContainer\"]/div/div[1]/div/div[2]/div[1]/div[1]/a"));
+            Assert.AreEqual(basketElement.Text, "Тостер Star-Light TS-800W, 800 W, 2 филии, Регулируема степен на изпичане, Функция размразяване, Инокс");
+
+            driver.Close();
+            driver.Quit();
+        }
+
+    }
+}
